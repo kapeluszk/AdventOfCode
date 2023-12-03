550ee8b34a4b771152e75e89867bf0525029ffda
kacper <126521076+kapeluszk@users.noreply.github.com>
2023-12-03 19:46:18 +0100

    day 1 ready

--- day1/main.go
+++ day1/main.go
@@ -8,6 +8,50 @@ import (
 	"strconv"
 )
 
+func convertToInt(s string) int {
+	stringToInt := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"enin":  9,
+		"thgie": 8,
+		"neves": 7,
+		"xis":   6,
+		"evif":  5,
+		"ruof":  4,
+		"eerht": 3,
+		"owt":   2,
+		"eno":   1,
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+	}
+
+	if stringToInt[s] != 0 || s == "0" {
+		return stringToInt[s]
+	} else if stringToInt[s[0:1]] != 0 || s[0:1] == "0" {
+		return stringToInt[s[0:1]]
+	}
+	res, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return res
+
+}
+
 func reverse(text string) (res string) {
 	for _, v := range text {
 		res = string(v) + res
@@ -15,18 +59,16 @@ func reverse(text string) (res string) {
 	return res
 }
 
-func findFirstInt(line string) string {
-	re := regexp.MustCompile("\\d")
-	digits := re.FindAllString(line, -1)
-	first := digits[0]
-	return first[0:1]
+func findFirstInt(line string) (res [2]int) {
+	re := regexp.MustCompile("(\\d|one|two|three|four|five|six|seven|eight|nine|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno)")
+	res[0] = convertToInt(re.FindString(line))
+	res[1] = convertToInt(re.FindString(reverse(line)))
+	return res
 }
 
 func findInt(line string) int {
-	digit1 := findFirstInt(line)
-	digit2 := findFirstInt(reverse(line))
-	concatenateDigits := digit1 + digit2
-	res, _ := strconv.Atoi(concatenateDigits)
+	digits := findFirstInt(line)
+	res := digits[0]*10 + digits[1]
 	return res
 }
 
@@ -43,7 +85,8 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		res += findInt(line)
-
 	}
 	fmt.Println(res)
+
+	fmt.Println(findInt("2911threeninesdvxvheightwobm"))
 }
